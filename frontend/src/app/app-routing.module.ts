8bc5fbea4478824f6930883e46ec78bacccadccf
Petko Todorov <p.todorov1015@gmail.com>
2024-02-24 15:02:09 +0200

    added registration to routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { CreateDestinationComponent } from './create-destination/create-destination.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "register", component: RegisterComponent  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
