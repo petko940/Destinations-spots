815cf20f127541e9c421bd868b5fd04c4788176f
Petko Todorov <p.todorov1015@gmail.com>
2024-02-24 14:59:32 +0200

    added users

---
+++ backend/apps/users/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], None, validated_data['password'])
+        print(user)
+        return user
