e986d257c2a0920869bea85ca9cfd0518192fc30
Petko Todorov <p.todorov1015@gmail.com>
2024-02-25 22:51:35 +0200

    added destinations

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.users.urls')),
+    path('', include('apps.destinations.urls')),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
