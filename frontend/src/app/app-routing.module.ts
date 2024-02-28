4371293c58105092b9344814beeacddbc824822a
Petko Todorov <p.todorov1015@gmail.com>
2024-02-28 22:06:47 +0200

    added info an redirect to home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { CreateDestinationComponent } from './create-destination/create-destination.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  
+  { path: "about", component: AboutComponent },
+
   { path: "create", component: CreateDestinationComponent },
 
   { path: "register", component: RegisterComponent },
