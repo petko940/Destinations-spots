3b5494e7c4ba16798d6153794bf4bfc37bd9454e
Petko Todorov <p.todorov1015@gmail.com>
2024-03-01 13:14:02 +0200

    Initial commit

---
+++ frontend/src/app/all-destinations/all-destinations.component.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Destination } from '../types/destination';
+
+@Component({
+    selector: 'app-all-destinations',
+    templateUrl: './all-destinations.component.html',
+    styleUrl: './all-destinations.component.css'
+})
+export class AllDestinationsComponent implements OnInit {
+    isLoading: boolean = true;
+    showMyDestinations: boolean = false;
+    allDestinations: Destination[] = [];
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+        this.http.get('http://127.0.0.1:8000/api/all-destinations/')
+            .subscribe((response: any) => {
+                this.isLoading = false
+                this.allDestinations = response;
+                console.log(this.allDestinations);
+            }, error => {
+                this.isLoading = false
+                console.log(error);
+            });
+    }
+
+    toggleShowMyDestinations() {
+        console.log(this.showMyDestinations);
+        this.showMyDestinations = !this.showMyDestinations
+    }
+
+}
