71ae26e721a501f2e4d2b0284360a24fe6889efe
Petko Todorov <p.todorov1015@gmail.com>
2024-03-02 11:28:59 +0200

    added SharedModule and changed/removed some components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,24 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
 import { RegisterComponent } from './register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CreateDestinationComponent } from './create-destination/create-destination.component';
-import { LoaderComponent } from './loader/loader.component';
+import { LoaderComponent } from './shared/loader/loader.component';
 import { AboutComponent } from './about/about.component';
 import { AllDestinationsComponent } from './all-destinations/all-destinations.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
     RegisterComponent,
     CreateDestinationComponent,
-    LoaderComponent,
     AboutComponent,
-    AllDestinationsComponent
+    AllDestinationsComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,6 +27,7 @@ import { AllDestinationsComponent } from './all-destinations/all-destinations.co
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
