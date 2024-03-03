087bf04ce1e52141e98d1a35699fc2b52d43e155
Petko Todorov <p.todorov1015@gmail.com>
2024-03-03 14:32:57 +0200

    Initial commit

---
+++ frontend/src/app/details-destination/details-destination.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-destination',
+  templateUrl: './details-destination.component.html',
+  styleUrl: './details-destination.component.css'
+})
+export class DetailsDestinationComponent {
+
+}
