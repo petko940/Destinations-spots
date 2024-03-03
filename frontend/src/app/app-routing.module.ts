cf468cce0a8c8e616bc1fc1b5b20ef6847541d46
Petko Todorov <p.todorov1015@gmail.com>
2024-03-03 14:33:57 +0200

    added DetailsDestinationComponent

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { RegisterComponent } from './register/register.component';
 import { CreateDestinationComponent } from './create-destination/create-destination.component';
 import { AboutComponent } from './about/about.component';
 import { AllDestinationsComponent } from './all-destinations/all-destinations.component';
+import { DetailsDestinationComponent } from './details-destination/details-destination.component';
 
 const routes: Routes = [
   { path: "about", component: AboutComponent },
   { path: "destinations", component: AllDestinationsComponent },
-
+  { path: "destination/:id", component: DetailsDestinationComponent },
   { path: "create", component: CreateDestinationComponent },
 
   { path: "register", component: RegisterComponent },
