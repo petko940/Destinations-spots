70e1c5e44f2bdda76308b3f4d00a1b22fe44bf1d
Petko Todorov <p.todorov1015@gmail.com>
2024-03-06 13:44:15 +0200

    added ratings on destinations and moved fetch from rating and destination to services

--- frontend/src/app/destinations/all-destinations/all-destinations.component.ts
+++ frontend/src/app/destinations/all-destinations/all-destinations.component.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Destination } from '../../types/destination';
 import { AuthenticationService } from '../../services/authentication.service';
+import { RatingService } from '../services/rating.service';
+import { Rating } from '../../types/rating';
+import { AllDestinationsService } from '../services/all-destinations.service';
 
 @Component({
     selector: 'app-all-destinations',
@@ -14,24 +16,62 @@ export class AllDestinationsComponent implements OnInit {
     showMyDestinations: boolean = false;
     allDestinations: Destination[] = [];
     myDestinations: Destination[] = [];
-    copyAllDestinations : Destination[] = [];
+    copyAllDestinations: Destination[] = [];
+    ratingsMap: { [destinationId: number]: Rating[] } = {};
+    filledStarsMap: { [destinationId: number]: number[] } = {};
+    remainingStarsMap: { [destinationId: number]: number[] } = {};
 
     constructor(
-        private http: HttpClient,
         private authenticationService: AuthenticationService,
         private router: Router,
+        private allDestinationsService: AllDestinationsService,
+        private ratingService: RatingService,
     ) { }
 
     ngOnInit() {
-        this.http.get('http://127.0.0.1:8000/api/all-destinations/')
+        this.fetchAllDestinations();
+    }
+
+    fetchAllDestinations() {
+        this.allDestinationsService.fetchAllDestinations()
             .subscribe((response: any) => {
                 this.isLoading = false;
                 this.allDestinations = response.sort((a: Destination, b: Destination) => b.id - a.id);
-                this.copyAllDestinations  = [...this.allDestinations];
+                this.copyAllDestinations = [...this.allDestinations];
+                this.fetchRatingsForDestinations();
+
             }, error => {
                 this.isLoading = false
                 console.log(error);
             });
+    }
+
+    fetchRatingsForDestinations() {
+        this.allDestinations.forEach(destination => {
+            this.ratingService.fetchRating(destination.id)
+                .subscribe(ratings => {
+                    this.ratingsMap[destination.id] = ratings;
+                    this.calculateRatings(destination.id);
+                }, error => {
+                    console.error('Error fetching ratings for destination:', destination.id, error);
+                });
+        });
+    }
+
+    calculateRatings(destinationId: number): void {
+        let averageRating = 0;
+        this.ratingsMap[destinationId].forEach(element => {
+            averageRating += element.stars;
+        });
+        if (averageRating === 0) {
+            this.filledStarsMap[destinationId] = [];
+            this.remainingStarsMap[destinationId] = Array(5).fill(0);
+            return
+        }
+        const filledStars = Array(Math.round(averageRating / this.ratingsMap[destinationId].length)).fill(0);
+        const remainingStars = Array(5 - filledStars.length).fill(0);
+        this.filledStarsMap[destinationId] = filledStars;
+        this.remainingStarsMap[destinationId] = remainingStars;
     }
 
     toggleShowMyDestinations() {
@@ -39,9 +79,9 @@ export class AllDestinationsComponent implements OnInit {
 
         if (this.showMyDestinations) {
             this.myDestinations = this.allDestinations.filter(destination => destination.user == this.authenticationService.getCurrentUser().id);
-            this.allDestinations = [...this.myDestinations]; 
+            this.allDestinations = [...this.myDestinations];
         } else {
-            this.allDestinations = [...this.copyAllDestinations]; 
+            this.allDestinations = [...this.copyAllDestinations];
         }
 
     }
