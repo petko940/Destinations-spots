aca0ca322e7673af486c38a85d9c24fe855912da
Petko Todorov <p.todorov1015@gmail.com>
2024-03-11 14:29:59 +0200

    many fixes

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -4,28 +4,30 @@ import { environment } from '../../environments/environment.development';
 import { tap } from 'rxjs';
 import { Router } from '@angular/router';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private isLoggedIn: boolean = false;
   tokenKey: string = 'token';
+  userKey: string = 'userid';
 
   constructor(
     private http: HttpClient,
     private router: Router,
-  ) {
-    this.isLoggedIn = !!this.getCookie(this.tokenKey);
-  }
+  ) { }
 
   login(credentials: { username: string, password: string }) {
     const apiUrl: string = environment.dbApiUrl;
     return this.http.post(apiUrl + 'login/', credentials).pipe(
       tap((response: any) => {
-        this.isLoggedIn = true;
         const expiresDate = new Date();
         expiresDate.setDate(expiresDate.getDate() + 7);
+
+        const encodedUsedId = encodeURIComponent(response.user_id);
         document.cookie = `token=${response.token}; expires=${expiresDate.toUTCString()}; path=/`;
+        document.cookie = `userid=${encodedUsedId}; expires=${expiresDate.toUTCString()}; path=/`;
+
       })
     );
   }
@@ -36,9 +38,17 @@ export class AuthService {
   }
 
   logout() {
-    this.isLoggedIn = false;
-    document.cookie = `${this.tokenKey}=; expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+    document.cookie = `${this.tokenKey}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+    document.cookie = `${this.userKey}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
     this.router.navigate(['/']);
+  }
+
+  getCurrentUserId() {
+    return this.getCookie('userid');
+  }
+
+  isLoggedIn() {
+    return !!this.getCookie(this.tokenKey);
   }
 
   getCookie(name: string) {
@@ -50,9 +60,5 @@ export class AuthService {
       }
     }
     return null;
-  }
-
-  isLoggedInUser() {
-    return this.isLoggedIn;
   }
 }
