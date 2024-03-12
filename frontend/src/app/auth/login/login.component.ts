fd2ca9c8bdb971215599cd2997a1159cdd0f95a8
Petko Todorov <p.todorov1015@gmail.com>
2024-03-12 14:05:14 +0200

    Initial commit

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import sha256 from 'sha256'; // Import SHA256 library
+import { Router } from '@angular/router';
+
 
 @Component({
     selector: 'app-login',
@@ -7,6 +11,47 @@ import { FormControl } from '@angular/forms';
     styleUrl: './login.component.css',
 })
 export class LoginComponent {
-    usernameFormControl = new FormControl('');
+    private username!: string;
+    private password!: string;
+
+    loginForm: FormGroup;
+
+    errorMessage: string = '';
+
+    constructor(
+        private authService: AuthService,
+        private fb: FormBuilder,
+        private router: Router,
+    ) {
+        this.loginForm = this.fb.group({
+            username: [''],
+            password: [''],
+        });
+    }
+
+    onSubmit() {        
+        if (!this.loginForm.value.username || !this.loginForm.value.password) {
+            this.errorMessage = 'Please enter both username and password';
+            return;
+        }
+
+        this.username = this.loginForm.value.username;
+        this.password = this.loginForm.value.password;
+
+        const hashedPassword = sha256(this.password);
+
+        const credentials = {
+            username: this.username,
+            password: hashedPassword,
+        };
+
+        this.authService.login(credentials)
+            .subscribe(() => {
+                this.router.navigate(['']);
+            }, error => {
+                console.log(error);
+                this.errorMessage = 'Invalid username or password';
+            })
+    }
 
 }
