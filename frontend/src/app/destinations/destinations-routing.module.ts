c29754aec9c471026f93d3522152e7fc1aba1644
Petko Todorov <p.todorov1015@gmail.com>
2024-03-13 19:12:08 +0200

    added edit destination

--- frontend/src/app/destinations/destinations-routing.module.ts
+++ frontend/src/app/destinations/destinations-routing.module.ts
@@ -4,11 +4,13 @@ import { AllDestinationsComponent } from "./all-destinations/all-destinations.co
 import { DetailsDestinationComponent } from "./details-destination/details-destination.component";
 import { CreateDestinationComponent } from "./create-destination/create-destination.component";
 import { AuthGuard } from "../auth/auth.guard";
+import { EditDestinationComponent } from "./edit-destination/edit-destination.component";
 
 const routes: Routes = [
+    { path: "create", component: CreateDestinationComponent, canActivate: [AuthGuard] },
     { path: "destinations", component: AllDestinationsComponent },
     { path: "destination/:id", component: DetailsDestinationComponent },
-    { path: "create", component: CreateDestinationComponent, canActivate: [AuthGuard] },
+    { path: "edit-destination/:id", component: EditDestinationComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
