d64af9b95293b9c8f6b5d19e90fece39f0a30679
Petko Todorov <p.todorov1015@gmail.com>
2024-03-13 19:11:46 +0200

    added modules

--- frontend/src/app/destinations/destinations.module.ts
+++ frontend/src/app/destinations/destinations.module.ts
@@ -2,33 +2,43 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateDestinationComponent } from './create-destination/create-destination.component';
 import { SharedModule } from '../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AllDestinationsComponent } from './all-destinations/all-destinations.component';
 import { DetailsDestinationComponent } from './details-destination/details-destination.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { DestinationsRoutingModule } from './destinations-routing.module';
-
-
+import { EditDestinationComponent } from './edit-destination/edit-destination.component';
+import { RouterModule } from '@angular/router';
+import { MatDialogModule, } from '@angular/material/dialog';
+import { DeleteDestinationComponent } from './delete-destination/delete-destination.component';
 
 @NgModule({
   declarations: [
     CreateDestinationComponent,
     AllDestinationsComponent,
     DetailsDestinationComponent,
+    EditDestinationComponent,
+    DeleteDestinationComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
     ReactiveFormsModule,
+    FormsModule,
     MatInputModule,
     MatFormFieldModule,
+    MatDialogModule,
+    RouterModule,
   ],
   exports: [
     CreateDestinationComponent,
     AllDestinationsComponent,
     DetailsDestinationComponent,
+    EditDestinationComponent,
+    DeleteDestinationComponent,
     DestinationsRoutingModule,
-  ]
+    MatDialogModule
+  ],
 })
 export class DestinationsModule { }
