da0d90d2350c8abe0cb75b14a873a10ad8b8a72a
Petko Todorov <p.todorov1015@gmail.com>
2024-03-14 18:13:20 +0200

    added destination

--- frontend/src/app/destinations/services/details-destination.service.ts
+++ frontend/src/app/destinations/services/details-destination.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Destination } from '../../types/destination';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailsDestinationService {
+  destination!: Destination;
   destinationId!: number;
 
   constructor() { }
