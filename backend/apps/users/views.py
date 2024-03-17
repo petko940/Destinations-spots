f98f51dfd7aa32e3e2373e8deb367700653f2df5
Petko Todorov <p.todorov1015@gmail.com>
2024-03-17 12:08:35 +0200

    changes made for jwt

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,11 +1,16 @@
-from django.contrib.auth import login, authenticate
+import jwt
+from django.contrib.auth import login, authenticate, get_user_model
 from rest_framework import generics, status
-from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework_simplejwt.models import TokenUser
+from rest_framework_simplejwt.settings import api_settings
 from rest_framework.views import APIView
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from apps.users.serializers import RegisterSerializer, LoginSerializer, MyTokenObtainPairSerializer
+from apps.users.serializers import RegisterSerializer, MyTokenObtainPairSerializer, UsernameSerializer
+
+User = get_user_model()
 
 
 # Create your views here.
@@ -21,15 +26,20 @@ class RegisterAPIView(generics.CreateAPIView):
             login(self.request, user)
 
 
-class LoginAPIView(APIView):
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-
-        if serializer.is_valid():
-            return Response(status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class ChangeUsernameView(generics.RetrieveUpdateAPIView):
+    serializer_class = UsernameSerializer
+    queryset = User.objects.all()
+
+    def put(self, request, *args, **kwargs):
+        user = self.get_object()
+        serializer = self.get_serializer(user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
