69662b09d9602af5d80b3320650738d9270551a9
Petko Todorov <p.todorov1015@gmail.com>
2024-03-17 12:10:10 +0200

    added edit-username

--- frontend/src/app/user/user-routing.module.ts
+++ frontend/src/app/user/user-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { EditUsernameComponent } from './edit-username/edit-username.component';
 
 const routes: Routes = [
   { path: "profile", component: ProfileComponent },
+  { path: "profile/edit-username", component: EditUsernameComponent },
 ];
 
 @NgModule({
