b3fd5f42e3ef5eaf679546f38d0327b1eaddfb5f
Petko Todorov <p.todorov1015@gmail.com>
2024-03-17 12:05:57 +0200

    added token refresh jwt

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from apps.users.views import RegisterAPIView, LoginAPIView, MyTokenObtainPairView
+from apps.users.views import RegisterAPIView, MyTokenObtainPairView, ChangeUsernameView
 
 urlpatterns = [
     path('api/register/', RegisterAPIView.as_view(), name='register'),
     path('api/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    path('api/user/<int:pk>/change-username/', ChangeUsernameView.as_view(), name='change-username'),
 ]
