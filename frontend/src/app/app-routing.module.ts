ad9df87616f528ec811e036712e98788b03de405
Petko Todorov <p.todorov1015@gmail.com>
2024-03-18 18:16:09 +0200

    added 404

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
+  { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
+
+  { path: '**', redirectTo: '404' },
+  { path: '404', component: ErrorComponent },
 ];
 
 @NgModule({
