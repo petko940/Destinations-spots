416e4ab4b107ded18b0b41235ff540174bd59041
Petko Todorov <p.todorov1015@gmail.com>
2024-03-20 12:43:58 +0200

    removed updateTokesn, made this on BE

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -32,51 +32,17 @@ export class AuthService {
     return this.http.post(this.apiUrl + 'register/', data);
   }
 
+  logout() {
+    document.cookie = `${this.accessTokenKey}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+    document.cookie = `${this.refreshTokenKey}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+    this.router.navigate(['/']);
+  }
+
   saveTokens(token: any) {
     const expiresDate = new Date();
     expiresDate.setDate(expiresDate.getDate() + 7);
     document.cookie = `${this.accessTokenKey}=${token.access}; expires=${expiresDate.toUTCString()}; path=/`;
     document.cookie = `${this.refreshTokenKey}=${token.refresh}; expires=${expiresDate.toUTCString()}; path=/`;
-  }
-
-  updateTokens(newUsername: string) {
-    const refreshToken = this.getCookie(this.refreshTokenKey);
-    const expiresDate = new Date();
-    expiresDate.setDate(expiresDate.getDate() + 7);
-
-    this.http.post(this.apiUrl + 'token-refresh/', { refresh: refreshToken })
-      .subscribe((response: any) => {
-        if (refreshToken) {
-          const newRefreshToken = response.refresh;
-          const decodedToken: any = jwtDecode(newRefreshToken);
-
-          decodedToken.username = newUsername;
-
-          const updatedPayload = this.encodePayload(decodedToken);
-          const [header, signature] = refreshToken.split('.');
-          const newToken = `${header}.${updatedPayload}.${signature}`;
-
-          document.cookie = `${this.accessTokenKey}=${newToken}; expires=${expiresDate.toUTCString()}; path=/`;
-          document.cookie = `${this.refreshTokenKey}=${newRefreshToken}; expires=${expiresDate.toUTCString()}; path=/`;
-        }
-      }, (error) => {
-        console.log(error);
-      })
-  }
-
-  private encodePayload(payload: any): string {
-    const encodedPayload = btoa(JSON.stringify(payload)); // Encoding payload using base64
-    return this.base64urlEncode(encodedPayload);
-  }
-
-  private base64urlEncode(input: string): string {
-    return input.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-  }
-
-  logout() {
-    document.cookie = `${this.accessTokenKey}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
-    document.cookie = `${this.refreshTokenKey}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
-    this.router.navigate(['/']);
   }
 
   getCurrentUserId() {
@@ -101,7 +67,7 @@ export class AuthService {
     return !!this.getCookie(this.accessTokenKey);
   }
 
-  fetchUser(userId: number) {
+  getUsername(userId: number) {
     return this.http.get<any>(`${this.apiUrl}user/${userId}`);
   }
 
