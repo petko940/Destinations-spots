a726f939221246f950686c08a5a58752f8b0c653
Petko Todorov <p.todorov1015@gmail.com>
2024-03-24 10:53:07 +0200

    added forms module

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -11,6 +12,7 @@ import { NavigationComponent } from './navigation/navigation.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule,
   ],
   exports: [
