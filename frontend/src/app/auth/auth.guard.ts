208f8a2aa48e578f7681c790def85edaadbb6f15
Petko Todorov <p.todorov1015@gmail.com>
2024-04-02 11:40:40 +0300

    prevent not logged user to create destination

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -13,10 +13,14 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      
+
     if (this.authService.isLoggedIn() &&
       (state.url === '/login' || state.url === '/register')) {
       return this.router.navigate(['/']);
+    }
+
+    if (!this.authService.isLoggedIn() && state.url === '/create') {
+      return this.router.navigate(['/login']);
     }
 
     return true;
